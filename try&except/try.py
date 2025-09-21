62a5f3ea34e1d538f35ce3f7b1760b661a1f911d
webbingWithData <ankitpandey2005kk@gmail.com>
2025-09-21 18:05:07 +0530

    adding try and except

---
+++ try&except/try.py
@@ -0,0 +1,14 @@
+# try:
+#     num1 = 10
+#     num2 = 0
+#     result = num1 / num2
+#     print("Result is:", result)
+# except:
+#     print("You can't devide a number by zero")
+
+
+try:
+    value = int(input("Enter a number: "))
+    print("You entered:", value)
+except:
+    print("Error: Please enter a valid integer!")
